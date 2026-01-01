9d512a124e1f3c69f2042099de8c916fb359da88
Sushan64 <sushan.khatiwada2064@gmail.com>
2026-01-01 14:21:58 +0545

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { SimpleEditor } from '@/components/tiptap-templates/simple/simple-editor'
+function App() {
+
+  return (
+    <>
+      <SimpleEditor />
+    </>
+  )
+}
+
+export default App
