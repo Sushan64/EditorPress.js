29076fc788b5d9c44495e20188b460f524664bdc
Sushan64 <sushan.khatiwada2064@gmail.com>
2026-01-03 14:50:51 +0545

    chore(navbar): Added editor name and credit in Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import { ToolbarContext } from "@/components/editor/context/ToolbarContext"
 function App() {
   return (
     <>
+      <nav className="fixed w-full px-5 bg-gray-500">
+        <h1 className="text-white text-2xl font-semibold">EditorPress.js <span className="text-sm font-normal underline"><a href="https://github.com/Sushan64" target="_blank">- by Sushan Khatiwada</a></span></h1>
+      </nav>
       <div className="h-auto min-h-dvh bg-gray-100 flex justify-center p-6">
       <ToolbarContext>
-      <Editor />
+        <Editor />
       </ToolbarContext>
       </div>
     </>
